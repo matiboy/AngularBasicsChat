b2a2764bdbc4a5089a44906e6c296f6d565347ae
Mathieu <mathieu@redapesolutions.com>
2013-10-16 17:59:19 +0800

    Put routes into place

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,39 @@
 'use strict';
 
-angular.module('ChatFrontendApp', ['ngRoute'])
+angular.module('ChatFrontendApp', ['ngRoute', 'btford.socket-io'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
+        controller: 'LoginCtrl as login'
+      })
+      .when('/chat', {
+        templateUrl: 'views/chat.html',
+        controller: 'ChatCtrl as chat',
+        resolve: {
+          socket: function(socket, $q, $timeout) {
+            var q = $q.defer();
+            socket.on('connected', function(data) {
+              q.resolve({
+                success: true,
+                id: data
+              });
+            });
+            // Promises are only resolved once, so this will only happen if we didn't recieve the connected event within 5s
+            $timeout( function() {
+              q.resolve({
+                success: false,
+                message: 'Time out'
+              });
+            },  5000);
+            return q.promise;
+          }
+        }
       })
       .otherwise({
         redirectTo: '/'
       });
+  }).config(function(socketProvider){
+    var socket = io.connect('http://192.168.1.137:5555');
+    socketProvider.ioSocket(socket);
   });
---
+++ app/scripts/controllers/chat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('ChatFrontendApp')
+  .controller('ChatCtrl', function ($scope, $route) {
+    // Received the resolved value of the promise defined in the route
+    var resolved = $route.current.locals.socket;
+    if(resolved.success) {
+      window.alert('You are now connected to the server with id ' + resolved.id);
+    } else {
+      window.alert('Unable to connect to server');
+    }
+  });
